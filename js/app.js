d22c1754b251f6403e93826246181b3d64c4b35a
Zerek <zerek.cover@gmail.com>
2019-08-15 13:49:15 -0700

    was able to get the dropdown populated

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const gallery = [];
 let pageOneImages = [];
 
+// ----- CONSTRUCTOR FUNCTION ---------
 function Image(url, title, desc, keyword, horns){
     this.url = url;
     this.title = title;
@@ -14,14 +15,10 @@ function Image(url, title, desc, keyword, horns){
     gallery.push(this);
 };
 
-// ----- CONSTRUCTOR FUNCTION ---------
+// -------EXECUTABLE HELPER-------
 const getAllImagesfromGallery = () => {
 
     $.get('./js/page-1.json').then(images => {
-        //console is logging the confirmation of image receieved from .json file
-        // console.log('Images received from .then', images);
-
-
         //forEach is looping once over each image to obtain the title,url,horns,description, and keyword properties of each image
         images.forEach(eachImage => {
             pageOneImages.push(new Image(
@@ -30,32 +27,16 @@ const getAllImagesfromGallery = () => {
                 eachImage.description, 
                 eachImage.keyword, 
                 eachImage.horns));
-        });
-        
+        }); 
     })   
     .then(data=>{
-        console.log('i am here');
-        console.log(pageOneImages);
+        // console.log('i am here');
+        // console.log(pageOneImages);
         renderImagesOne();
-        console.log(data);
+        renderDropdown();
+        // console.log(data);
     });
 };
-
-let renderImagesOne = function(){
-
-    console.log('I am at the for loop')
-    console.log(pageOneImages)
-    pageOneImages.forEach(object=>{
-        console.log('inside of the for loop')
-        object.renderWithjQuery();
-    })
-    console.log('I am after the for loop');
-}
-console.log('preloop')
-// console.log('render with query')
-
-//for every object in the gallery arr we will renderWithjQuery
-
 // PROTOTYPES
 //renderWithjQuery identifies DOM, creates a <section> fills with data and appends to main. 
 Image.prototype.renderWithjQuery = function(){
@@ -80,40 +61,31 @@ Image.prototype.renderWithjQuery = function(){
     $('main').append($newSection);
 };
 
+//////////////////////////////////////
 
-// Image.prototype.renderDropDown = function(keyword){
-//     const $myMenu = $('#menu');
-//     const $myMenuHtml = $myMenu.html();
-
-//     const $newMenu = $('<option></option>');
-//     $newMenu.html($myMenuHtml);
-
-//     $newMenu.find('option').html(this.keyword);
-
-//     //append
-//     $('option').append($newMenu);
-//     console.log('hi');
-// }
+const renderDropdown = () => {
+    pageOneImages.forEach(function(element) {
+        $('#menu').append(`<option>${element.keyword}</option>`);
+    });
+}
 
 
-// let example = function(){
-//     const $exDropDown = $('#menu');
-//     const $templateHtml = $exDropDown.html();
-//     //created template of drop down menu
-//     const $newSelect = $('<select></select>');
-    
-//     $newSelect.find('option').text('1');
-//     $('select'.child).append()
-// }
 
 
+
+
+
+let renderImagesOne = function(){
+
+    // console.log('I am at the for loop')
+    // console.log(pageOneImages)
+    pageOneImages.forEach(object=>{
+        // console.log('inside of the for loop')
+        object.renderWithjQuery();
+    });
+    // console.log('I am after the for loop');
+}
+// console.log('preloop');
 
 
 getAllImagesfromGallery();
-
-console.log('get all images')
-// renderDropDown();
-console.log('render drop down');
-
-// render
-console.log();
